95e24be88b073295a32117e32f9da9f0d90582fc
hema2718 <hemachinnu273@gmail.com>
2026-07-30 12:07:14 +0530

    added python data types example

---
+++ python/datatypes.py
@@ -0,0 +1,9 @@
+name = "Hema"        # String
+age = 20             # Integer
+height = 5.4         # Float
+is_student = True    # Boolean
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
