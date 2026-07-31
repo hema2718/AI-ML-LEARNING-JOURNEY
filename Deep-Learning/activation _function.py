e9888b4826ced3f619487d68c8a148d415693a88
Hemalatha G <163004567+hema2718@users.noreply.github.com>
2026-07-31 13:43:24 +0530

    added activation function example

---
+++ Deep-Learning/activation _function.py
@@ -0,0 +1,8 @@
+print("Activation Function")
+
+print("An activation function decides whether a neuron should be activated or not.")
+
+print("\nExamples:")
+print("- ReLU")
+print("- Sigmoid")
+print("- Tanh")
