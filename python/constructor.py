45f0e94093f73b57687eff28b9135b50f93b6b70
Hemalatha G <163004567+hema2718@users.noreply.github.com>
2026-07-31 12:06:13 +0530

    added constructor example

---
+++ python/constructor.py
@@ -0,0 +1,9 @@
+class Student:
+    def __init__(self, name, cgpa):
+        self.name = name
+        self.cgpa = cgpa
+
+student1 = Student("Hema", 8.32)
+
+print(student1.name)
+print(student1.cgpa)
