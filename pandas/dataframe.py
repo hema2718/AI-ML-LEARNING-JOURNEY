08a8a1f589b2a0297d06a2f7fa87435a8abd374c
Hemalatha G <163004567+hema2718@users.noreply.github.com>
2026-07-31 12:47:10 +0530

    added pandas dataframe example

---
+++ pandas/dataframe.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+student = {
+    "Name": ["Hema", "Rahul", "Priya"],
+    "Branch": ["ISE", "CSE", "AIML"],
+    "CGPA": [8.32, 8.75, 9.10]
+}
+
+df = pd.DataFrame(student)
+
+print(df)
