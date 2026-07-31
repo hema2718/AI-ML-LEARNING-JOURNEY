75ad686e1d6fde51a537ba781184d1588de8a89e
Hemalatha G <163004567+hema2718@users.noreply.github.com>
2026-07-31 13:36:12 +0530

    added deeplearning introduction

---
+++ Deep-Learning/introduction.py
@@ -0,0 +1,5 @@
+print("Deep Learning")
+print("----------------")
+
+print("Deep Learning is a branch of Machine Learning.")
+print("It uses Artificial Neural Networks (ANNs) to learn from large amounts of data.")
