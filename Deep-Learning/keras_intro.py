188fa12232a10048fcccec714be83d8bc4e3b827
Hemalatha G <163004567+hema2718@users.noreply.github.com>
2026-07-31 13:47:52 +0530

    added keras example

---
+++ Deep-Learning/keras_intro.py
@@ -0,0 +1,7 @@
+from tensorflow import keras
+
+print("Keras is a high-level API for building Deep Learning models.")
+
+model = keras.Sequential()
+
+print("Empty Sequential Model Created Successfully!")
