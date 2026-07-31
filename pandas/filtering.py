e5b8d86925a669946e2822542ceb5012f62d1f58
Hemalatha G <163004567+hema2718@users.noreply.github.com>
2026-07-31 12:51:32 +0530

    added pandas filtering example

---
+++ pandas/filtering.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+student = {
+    "Name": ["Hema", "Rahul", "Priya", "Anjali"],
+    "Branch": ["ISE", "CSE", "AIML", "ECE"],
+    "CGPA": [8.32, 8.75, 9.10, 7.95]
+}
+
+df = pd.DataFrame(student)
+
+print(df[df["CGPA"] >= 8.5])
