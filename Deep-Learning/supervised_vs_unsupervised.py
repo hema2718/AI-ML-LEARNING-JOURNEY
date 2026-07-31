0756f782520c96f513222a0b718bab597cdd16f6
Hemalatha G <163004567+hema2718@users.noreply.github.com>
2026-07-31 13:38:06 +0530

    added supervised and unsupervised learning comparsion

---
+++ Deep-Learning/supervised_vs_unsupervised.py
@@ -0,0 +1,9 @@
+print("Supervised Learning")
+print("- Uses labeled data")
+print("- Predicts outputs")
+
+print()
+
+print("Unsupervised Learning")
+print("- Uses unlabeled data")
+print("- Finds hidden patterns and groups")
