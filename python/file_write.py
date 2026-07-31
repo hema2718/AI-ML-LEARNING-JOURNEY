d0fd3d0866f28c9fa14ed521f0dc78443e02162a
Hemalatha G <163004567+hema2718@users.noreply.github.com>
2026-07-31 11:49:32 +0530

    addes  file writing example

---
+++ python/file_write.py
@@ -0,0 +1,9 @@
+file = open("student.txt", "w")
+
+file.write("Name: Hema\n")
+file.write("Course: AI & Machine Learning\n")
+file.write("Branch: ISE")
+
+file.close()
+
+print("Data written successfully!")
