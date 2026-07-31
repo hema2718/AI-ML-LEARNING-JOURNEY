27b784df6beef1233947557f6a5201c642d3f643
Hemalatha G <163004567+hema2718@users.noreply.github.com>
2026-07-31 11:47:06 +0530

    added sets example

---
+++ python/sets.py
@@ -0,0 +1,9 @@
+numbers = {10, 20, 30, 20, 40, 10}
+
+print("Original Set:", numbers)
+
+numbers.add(50)
+print("After adding 50:", numbers)
+
+numbers.remove(30)
+print("After removing 30:", numbers)
