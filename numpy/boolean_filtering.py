4159bcda437b660bca2bd8eabf08b25d2ab9e382
Hemalatha G <163004567+hema2718@users.noreply.github.com>
2026-07-31 12:24:15 +0530

    added numpy boolean filtiring example

---
+++ numpy/boolean_filtering.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+marks = np.array([45, 60, 75, 30, 90])
+
+print(marks)
+
+print("Marks greater than 50:")
+print(marks[marks > 50])
+
+print("Marks less than 50:")
+print(marks[marks < 50])
